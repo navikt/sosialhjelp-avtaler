c64b9ba9854d56aadf0ea958d3f3bfcf3da4af4c
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-09-26 15:21:43 +0200

    blafring

--- client/src/DigisosAvtale.tsx
+++ client/src/DigisosAvtale.tsx
@@ -3,15 +3,29 @@ import { Avtale, OpprettAvtale, OpprettAvtaleRequest } from './types';
 import { useGet } from './api/useGet';
 import { usePost } from './api/usePost';
 import React, { ReactNode, useEffect } from 'react';
+import { Loader } from '@navikt/ds-react';
 
+const CenteredContent = styled.div`
+  width: 100%;
+  padding: 40px;
+  text-align: center;
+`;
 export function DigisosAvtale() {
-  const { data: avtale } = useGet<Avtale>('/avtale/Line');
+  const { data: avtale, error } = useGet<Avtale>('/avtale/Line');
   const { post: post, data: postResponse } = usePost<OpprettAvtale, Avtale>('/avtale');
 
   useEffect(() => {
     const avtale: OpprettAvtale = { navn: 'Ida' };
     post(avtale).catch((e) => console.log(e));
   }, []);
+
+  if (!avtale && !error) {
+    return (
+      <CenteredContent>
+        <Loader size="large" />
+      </CenteredContent>
+    );
+  }
 
   if (avtale) {
     return (
@@ -23,6 +37,5 @@ export function DigisosAvtale() {
       </main>
     );
   }
-
-  return <>Noe gikk galt..</>;
+  return null;
 }
