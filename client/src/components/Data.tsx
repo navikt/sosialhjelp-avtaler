2f8f4054dbef066d2056058b661aa94dc4f473af
Ida <25682301+idaons@users.noreply.github.com>
2022-12-09 11:17:24 +0100

    fikser slik at zoom til dobbel fontstørrelse funker (#32)

--- client/src/components/Data.tsx
+++ client/src/components/Data.tsx
@@ -1,16 +1,20 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { enhet } from '../styles/enhet';
 
 export const Data = styled.dl`
   display: grid;
-  grid-template-columns: 150px auto;
+  grid-template-columns: min-content auto;
   gap: var(--a-spacing-1) var(--a-spacing-2);
   margin: 0;
   align-items: center;
+  dd {
+    margin-inline-start: 20px;
+  }
 
-  @media only screen and (max-width: 500px) {
+  @media ${enhet.mobil} {
     grid-template-columns: auto;
     dd {
       margin-inline-start: 0;
     }
   }
-`
+`;
--- client/src/styles/enhet.ts
+++ client/src/styles/enhet.ts
@@ -1,5 +1,4 @@
 export const enhet = {
-  desktop: `only screen and (min-width: 801px)`,
-  tablet: `only screen and (max-width: 800px)`,
-  mobil: `only screen and (max-width: 500px)`,
-}
+  desktop: `only screen and (min-width: 50em)`,
+  mobil: `only screen and (max-width: 32em)`,
+};
