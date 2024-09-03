3dec367be2c92801697127b373770863f4e84c5e
Martin Almvik <martin.almvik@nav.no>
2024-09-03 10:20:07 +0200

    Justerer på layout på kvitteringside

--- client/src/avtale/AvtaleKvittering.tsx
+++ client/src/avtale/AvtaleKvittering.tsx
@@ -8,7 +8,6 @@ import styled from 'styled-components/macro';
 import useBreadcrumbs from '../components/hooks/useBreadcrumbs';
 import { usePageTitle } from '../components/hooks/usePageTitle';
 import { useGet } from '../api/useGet';
-import { AppLink } from '../components/AppLink';
 
 export function AvtaleKvittering() {
   const {
@@ -33,16 +32,8 @@ export function AvtaleKvittering() {
         {t('avtale.kvittering_for', { navn: kommune.navn })}
       </Heading>
       <Alert variant="success">{t('avtale.suksess')}</Alert>
+      <AvtalePanel avtale={kommune} />
       <Avstand marginBottom={5} />
-      {kommune.erSignert && (
-        <BodyLong spacing>
-          {
-            <AppLink target="_blank" href={`/api/avtale/${kommune.uuid}/signert-avtale`}>
-              {t('avtale.last_ned')}
-            </AppLink>
-          }
-        </BodyLong>
-      )}
       {kommune.kvitteringstekst && language === 'nb' && (
         <BodyLong spacing style={{ whiteSpace: 'pre' }}>
           {kommune.kvitteringstekst}
@@ -53,8 +44,8 @@ export function AvtaleKvittering() {
           {kommune.kvitteringstekstNynorsk}
         </BodyLong>
       )}
+      <Avstand marginBottom={5} />
       <StyledReadMore header={t('personopplysninger.overskrift')}>{t('personopplysninger.detaljer')}</StyledReadMore>
-      <AvtalePanel avtale={kommune} />
       <Avstand marginBottom={5} />
       <Link to="/" className={'navds-link'}>
         {t('avtale.lenke_tilbake_til_forsiden')}
