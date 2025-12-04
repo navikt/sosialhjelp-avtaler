73669811785e892149ed5fe88cd90a6b099ecb2e
joeldlin <jorgen.elden.lindgren@nav.no>
2025-12-04 15:40:05 +0100

    wip

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,15 @@
+/* eslint-env node */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transformIgnorePatterns: [
+    // Allow transformation of ESM modules in pnpm's .pnpm directory
+    'node_modules/(?!.pnpm|@navikt)',
+  ],
+  moduleNameMapper: {
+    // Mock parse5 which is ESM-only
+    '^parse5$': '<rootDir>/__mocks__/parse5.js',
+  },
 };
